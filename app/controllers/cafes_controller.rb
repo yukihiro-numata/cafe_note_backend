8eb1f1e519633bd551e8e3bf80cf7838f7f39355
yukihiro-numata <yuki.numata97@gmail.com>
2022-04-06 23:32:34 +0900

    Add post cafe.

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -9,4 +9,31 @@ class CafesController < ApplicationController
     cafe = Cafe.find(params[:id])
     render json: { status: "SUCCESS", message: "Loaded the cafe", data: cafe }
   end
+
+  def create
+    cafe = Cafe.create!(cafe_params)
+    render json: { status: "SUCCESS", message: "Created the cafe", data: cafe }
+  end
+
+  private
+  def cafe_params
+    params
+      .require(:cafe)
+      .permit(
+        :name,
+        :address,
+        :nearest_station,
+        :transportation,
+        :business_hours,
+        :regular_holiday,
+        :can_takeout,
+        :has_parking,
+        :has_wifi,
+        :has_power_supply,
+        :can_smoking,
+        :memo,
+        :img_path,
+        :tabelog_url
+      )
+  end
 end
