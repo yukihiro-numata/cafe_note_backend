6e20c33dece12927ec0395503eb8dd94385aded6
yukihiro-numata <yuki.numata97@gmail.com>
2022-09-21 21:10:26 +0900

    Update cafe media form spec

--- spec/forms/cafe_media_form_spec.rb
+++ spec/forms/cafe_media_form_spec.rb
@@ -7,15 +7,47 @@ RSpec.describe CafeMediaForm do
 
       context 'normal' do
         let!(:media_type) { :google_map }
-        it 'should return true' do
+        it 'valid? should return true' do
           expect(subject).to be_truthy
         end
       end
 
       context 'abnormal' do
-        let!(:media_type) { 'test' }
-        it 'should return false' do
-          expect(subject).to be_falsey
+        it 'valid? should return false without argument' do
+          expect(CafeMediaForm.new(url: 'https://example.com').valid?).to be_falsey
+        end
+
+        where(:media_type) { %W[test #{}] }
+        with_them do
+          it 'valid? should return false with invalid argument' do
+            expect(subject).to be_falsey
+          end
+        end
+      end
+    end
+
+    describe 'url' do
+      subject { CafeMediaForm.new(media_type: :google_map, url: url).valid? }
+
+      context 'normal' do
+        where(:url) { %w[https://example.com http://example.com https://example.com/test https://example.com?foo=bar] }
+        with_them do
+          it 'valid? should return true' do
+            expect(subject).to be_truthy
+          end
+        end
+      end
+
+      context 'abnormal' do
+        it 'valid? should return false without argument' do
+          expect(CafeMediaForm.new(media_type: :google_map).valid?).to be_falsey
+        end
+
+        where(:url) { %W[example.com http//example.com htt://example.com #{}] }
+        with_them do
+          it 'valid? should return false with invalid argument' do
+            expect(subject).to be_falsey
+          end
         end
       end
     end
