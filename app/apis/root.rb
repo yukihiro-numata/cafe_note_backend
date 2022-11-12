214a8ea1065820bc1944591760bebf50b718dcad
yukihiro-numata <yuki.numata97@gmail.com>
2022-11-12 23:37:47 +0900

    Update to return 404 when record not found.

--- app/apis/root.rb
+++ app/apis/root.rb
@@ -1,6 +1,10 @@
 class Root < Grape::API
   format :json
 
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    rack_response({ message:e.message, status: 404 }.to_json, 404)
+  end
+
   mount Resources::Users
   mount Resources::Cafes
 end
