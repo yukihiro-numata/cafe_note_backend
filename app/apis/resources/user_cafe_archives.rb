5e86ec231b65c689a62b56b5da2ff21293e9eecd
yukihiro-numata <yuki.numata97@gmail.com>
2022-11-19 13:21:20 +0900

    Add resource to get a user cafe archive.

--- app/apis/resources/user_cafe_archives.rb
+++ app/apis/resources/user_cafe_archives.rb
@@ -9,6 +9,18 @@ module Resources
         @user_cafe_archives = UserCafeArchive.includes([:cafe]).order(created_at: :desc).where(user_id: params[:user_id])
         present @user_cafe_archives, with: Entities::UserCafeArchiveEntity
       end
+
+      desc 'return an user cafe archive'
+      params do
+        requires :user_id, type: Integer # TODO: パラメータではなくトークンから引っ張るようにする
+        requires :user_cafe_archive_id, type: Integer
+      end
+      get '/:user_cafe_archive_id' do
+        @user_cafe_archive = UserCafeArchive.includes(%i[cafe user_cafe_archive_images]).find_by!(
+          id: params[:user_cafe_archive_id], user_id: params[:user_id]
+        )
+        present @user_cafe_archive, with: Entities::UserCafeArchiveEntity
+      end
     end
   end
 end
--- spec/apis/user_cafe_archives_spec.rb
+++ spec/apis/user_cafe_archives_spec.rb
@@ -34,5 +34,64 @@ describe 'api user cafe archives', type: :request do
         end
       end
     end
+
+    describe 'with id' do
+      subject { get "/user_cafe_archives/#{cafe_archive_id}", params: { user_id: user_1.id } }
+      let!(:user_1) { create(:user, email: 'aaa@example.com') }
+      let!(:user_2) { create(:user, email: 'bbb@example.com') }
+      let!(:cafe) { create(:cafe) }
+      let!(:cafe_archives_1) do
+        create(:user_cafe_archive, user: user_1, cafe: cafe).tap do |a|
+          create(:user_cafe_archive_image, user_cafe_archive: a)
+        end
+      end
+      let!(:cafe_archives_2) do
+        create(:user_cafe_archive, user: user_2, cafe: cafe).tap do |a|
+          create(:user_cafe_archive_image, user_cafe_archive: a)
+        end
+      end
+      context '正常系' do
+        let!(:cafe_archive_id) { cafe_archives_1.id }
+        it 'リクエストに成功すること' do
+          subject
+          expect(response).to have_http_status 200
+        end
+        it 'データ取得できること' do
+          subject
+          expect(response.body).to be_json_including(
+            {
+              'id' => Integer,
+              'cafe_id' => Integer,
+              'rating' => Integer,
+              'memo' => String,
+              'visited_at' => String,
+              'user_cafe_archive_images' => [
+                {
+                  'id' => Integer,
+                  'image_path' => String,
+                }
+              ],
+            }
+          )
+          expect(JSON.parse(response.body)['id']).to eq cafe_archives_1.id
+        end
+      end
+      context '異常系' do
+        context '存在しないIDが指定された場合' do
+          let!(:cafe_archive_id) { 9999 }
+          it '404が返ること' do
+            subject
+            expect(response).to have_http_status 404
+          end
+        end
+        context '対象のユーザーでないユーザーのデータが指定された場合' do
+          let!(:cafe_archive_id) { cafe_archives_2.id }
+          it '404が返ること' do
+            subject
+            expect(response).to have_http_status 404
+          end
+        end
+      end
+    end
   end
 end
