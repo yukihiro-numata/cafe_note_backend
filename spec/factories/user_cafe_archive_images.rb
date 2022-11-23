52f94367647ce77ccf672c83d8ad2ff2cf7168ad
yukihiro-numata <yuki.numata97@gmail.com>
2022-11-24 00:27:28 +0900

    Update annotates.

--- spec/factories/user_cafe_archive_images.rb
+++ spec/factories/user_cafe_archive_images.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: user_cafe_archive_images
+#
+#  id                   :bigint           not null, primary key
+#  image_path           :string(255)      not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  user_cafe_archive_id :bigint           not null
+#
+# Indexes
+#
+#  index_user_cafe_archive_images_on_user_cafe_archive_id  (user_cafe_archive_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_cafe_archive_id => user_cafe_archives.id)
+#
 FactoryBot.define do
   factory :user_cafe_archive_image do
     image_path { '/sample.jpeg' }
