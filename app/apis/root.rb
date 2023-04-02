74846c5ba742e793e36ab70d2a69657b608ee0f3
yukihiro-numata <yuki.numata97@gmail.com>
2023-04-02 18:27:40 +0900

    Update to respond reason of validation failure.

--- app/apis/root.rb
+++ app/apis/root.rb
@@ -6,6 +6,9 @@ class Root < Grape::API
   rescue_from ActiveRecord::RecordNotFound do |e|
     rack_response({ message: e.message, status: 404 }.to_json, 404)
   end
+  rescue_from Grape::Exceptions::ValidationErrors do |e|
+    rack_response({ message: "invalid arguments, #{e.message}", status: 400 }.to_json, 400)
+  end
 
   mount Resources::Users
   mount Resources::Cafes
